a2e7c84ca444588d210962f2001b94d3d62911ca
Steve Kotsopoulos <sk@fywss.com>
2019-07-19 13:41:42 -0400

    started integrating to mysql

--- robots/README.md
+++ robots/README.md
@@ -4,7 +4,7 @@ A fun project to re-write a node.js application in Golang
 
 ## Setup Project Dependencies
 
-go get -v github.com/gin-gonic/gin github.com/stretchr/testify/assert
+go get -v github.com/gin-gonic/gin github.com/jinzhu/gorm github.com/go-sql-driver/mysql github.com/stretchr/testify/assert
 
 ## Testing
 
@@ -14,6 +14,13 @@ go test
 
 go run robots.go
 curl http://localhost:3000/ping
+
+# Insert some test data
+
+insert into todo (title, url, completed) values ('Vegan Lentil Taco Salad Bowls', 'http://thefitchen.com/vegan-lentil-taco-salad-bowls-2/', 1);
+insert into todo (title, url, completed) values ('Bbq chicken shawarma', 'https://www.feastingathome.com/grilled-chicken-shawarma-recipe/', 5);
+insert into todo (title, url, completed) values ('Amazing chicken tacos', 'http://www.onceuponachef.com/2011/02/chicken-tacos.html"', 5);
+
 
 # Todo
 
--- robots/robots.go
+++ robots/robots.go
@@ -1,57 +1,93 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+type (
+	todo struct {
+		gorm.Model
+		Title     string `json:"title"`
+		URL       string `json:"url"`
+		Completed int    `json:"completed"`
+	}
+	// transformedTodo represents a formatted todo
+	transformedTodo struct {
+		ID        uint   `json:"id"`
+		Title     string `json:"title"`
+		URL       string `json:"url"`
+		Completed bool   `json:"completed"`
+	}
+)
+
+var db *gorm.DB
+
+func init() {
+	dbPasswd, ok := os.LookupEnv("ROBOTS_PASS")
+	if !ok {
+		panic("must set ROBOTS_PASS in environment")
+	}
+	//open a db connection
+	var err error
+	db, err = gorm.Open("mysql", "robotron:"+dbPasswd+"@tcp(localhost:3306)/robots?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	//Migrate the schema
+	db.SingularTable(true)
+	db.AutoMigrate(&todo{})
+}
+
 func main() {
-	r := setupRouter()
-	r.Run(":3000") // listen and serve on 0.0.0.0:3000
+	router := setupRouter()
+	router.Run(":3000") // listen and serve on 0.0.0.0:3000
 }
 
 func setupRouter() *gin.Engine {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.GET("/user/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.String(http.StatusOK, "Hello %s\n", name)
-	})
-	r.POST("/form", func(c *gin.Context) {
-		message := c.PostForm("message")
-		nick := c.DefaultPostForm("nick", "anonymous")
 
-		c.JSON(200, gin.H{
-			"status":  "posted",
-			"message": message,
-			"nick":    nick,
-		})
-	})
-	r.GET("/someXML", func(c *gin.Context) {
-		c.XML(http.StatusOK, gin.H{"message": "hey", "status": http.StatusOK})
-	})
+	v1 := router.Group("/robots/api/v1.0/recipes")
+	{
+		// v1.POST("/", createRecipe)
+		v1.GET("/", fetchAllRecipe)
+		// v1.GET("/:id", fetchSingleRecipe)
+		//v1.PUT("/:id", updateRecipe)
+		// v1.DELETE("/:id", deleteRecipe)
+	}
 
-	r.GET("/someYAML", func(c *gin.Context) {
-		c.YAML(http.StatusOK, gin.H{"message": "hey", "status": http.StatusOK})
-	})
-	r.StaticFile("/resolv", "/etc/resolv.conf")
-	r.GET("/cookie", func(c *gin.Context) {
+	return router
+}
 
-		cookie, err := c.Cookie("gin_cookie")
+func fetchAllRecipe(c *gin.Context) {
+	var todos []todo
+	var _todos []transformedTodo
 
-		if err != nil {
-			cookie = "NotSet"
-			c.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
+	db.Find(&todos)
+
+	if len(todos) <= 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
+		return
+	}
+
+	//transforms the todos for building a good response
+	for _, item := range todos {
+		completed := false
+		if item.Completed == 1 {
+			completed = true
+		} else {
+			completed = false
 		}
-
-		fmt.Printf("Cookie value: %s \n", cookie)
-	})
-
-	return r
+		_todos = append(_todos, transformedTodo{ID: item.ID, Title: item.Title, URL: item.URL, Completed: completed})
+	}
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todos})
 }
